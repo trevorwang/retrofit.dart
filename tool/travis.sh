0ae74584360892d8c2371c132d742b04acba3667
Trevor Wang <trevor.wang@qq.com>
2019-04-23 11:25:15 +0800

    enable travis ci (#4)

---
+++ tool/travis.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Created with package:mono_repo v2.0.0
+
+if [[ -z ${PKGS} ]]; then
+  echo -e '\033[31mPKGS environment variable must be set!\033[0m'
+  exit 1
+fi
+
+if [[ "$#" == "0" ]]; then
+  echo -e '\033[31mAt least one task argument must be provided!\033[0m'
+  exit 1
+fi
+
+EXIT_CODE=0
+
+for PKG in ${PKGS}; do
+  echo -e "\033[1mPKG: ${PKG}\033[22m"
+  pushd "${PKG}" || exit $?
+  pub upgrade --no-precompile || exit $?
+
+  for TASK in "$@"; do
+    case ${TASK} in
+    dartanalyzer) echo
+      echo -e '\033[1mTASK: dartanalyzer\033[22m'
+      echo -e 'dartanalyzer .'
+      dartanalyzer . || EXIT_CODE=$?
+      ;;
+    *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
+      EXIT_CODE=1
+      ;;
+    esac
+  done
+
+  popd
+done
+
+exit ${EXIT_CODE}
