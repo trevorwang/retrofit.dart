1aa99f8eb94ebe8ec364ec94cfc39ea6abaecd7d
Trevor Wang <trevor.wang@qq.com>
2019-05-27 10:17:41 +0800

    rename queryMap to queries (#15)

--- generator/example/example.g.dart
+++ generator/example/example.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'demo.dart';
+part of 'example.dart';
 
 // **************************************************************************
 // RetrofitGenerator
