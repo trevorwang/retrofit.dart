ac2cf5cabd2d21f367137c7c860f9bbe810ed2be
Trevor Wang <trevor.wang@qq.com>
2019-06-19 00:04:03 +0800

    test: add unit test for RestApi
    related: #10

---
+++ generator/test/generator_test.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+import 'package:retrofit_generator/src/generator.dart';
+import 'package:source_gen_test/src/build_log_tracking.dart';
+import 'package:source_gen_test/src/init_library_reader.dart';
+import 'package:source_gen_test/src/test_annotated_classes.dart';
+
+import 'package:retrofit/http.dart' as http;
+
+Future<void> main() async {
+  final reader = await initializeLibraryReaderForDirectory(
+      'test/src', 'generator_test_src.dart');
+  initializeBuildLogTracking();
+  testAnnotatedElements<http.RestApi>(reader, RetrofitGenerator());
+}
---
+++ generator/test/src/generator_test_src.dart
@@ -0,0 +1,31 @@
+import 'package:source_gen_test/annotations.dart';
+import 'package:retrofit/http.dart';
+
+@ShouldGenerate(
+  r'''
+class _RestClient implements RestClient {
+  _RestClient([this._dio]) {
+    ArgumentError.checkNotNull(_dio, '_dio');
+  }
+
+  final Dio _dio;
+}
+''',
+)
+@RestApi()
+abstract class RestClient {}
+
+@ShouldGenerate(
+  r'''
+class _BaseUrl implements BaseUrl {
+  _BaseUrl([this._dio]) {
+    ArgumentError.checkNotNull(_dio, '_dio');
+    _dio.options.baseUrl = 'http://httpbin.org/';
+  }
+
+  final Dio _dio;
+}
+''',
+)
+@RestApi(baseUrl: "http://httpbin.org/")
+abstract class BaseUrl {}
