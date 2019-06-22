3ef0c1cd6f22b627948e74621858ea59e1149c37
devkabiir <dev@kabiir.me>
2019-06-22 18:18:14 +0530

    ✅test (generator) Add tests for `@dio.Extra` annotation

--- generator/test/src/generator_test_src.dart
+++ generator/test/src/generator_test_src.dart
@@ -1,5 +1,6 @@
 import 'package:source_gen_test/annotations.dart';
 import 'package:retrofit/http.dart';
+import 'package:retrofit/dio.dart' as dio;
 
 @ShouldGenerate(
   r'''
@@ -29,3 +30,46 @@ class _BaseUrl implements BaseUrl {
 )
 @RestApi(baseUrl: "http://httpbin.org/")
 abstract class BaseUrl {}
+
+@ShouldGenerate(
+  r'''
+    const _extra = <String, dynamic>{};
+''',
+  contains: true,
+)
+@RestApi()
+abstract class EmptyExtras {
+  @GET('/list/')
+  @dio.Extra({})
+  Future<void> list();
+}
+
+@ShouldGenerate(
+  r'''
+    const _extra = <String, dynamic>{'key': 'value'};
+''',
+  contains: true,
+)
+@RestApi()
+abstract class ExtrasWithPrimitiveValues {
+  @GET('/list/')
+  @dio.Extra({'key': 'value'})
+  Future<void> list();
+}
+
+@ShouldGenerate(
+  r'''
+    const _extra = <String, dynamic>{'key': CustomConstant()};
+''',
+  contains: true,
+)
+@RestApi()
+abstract class ExtrasWithCustomConstant {
+  @GET('/list/')
+  @dio.Extra({'key': CustomConstant()})
+  Future<void> list();
+}
+
+class CustomConstant {
+  const CustomConstant();
+}
