4f118348ec8733fc98e84f03d5bc9aa1522f32d1
Trevor Wang <Trevor.Wang@qq.com>
2019-11-01 11:44:18 +0800

    fix: fix compile warning

--- example/lib/example.g.dart
+++ example/lib/example.g.dart
@@ -29,11 +29,12 @@ Map<String, dynamic> _$TaskToJson(Task instance) => <String, dynamic>{
 class _RestClient implements RestClient {
   _RestClient(this._dio, {this.baseUrl}) {
     ArgumentError.checkNotNull(_dio, '_dio');
+    this.baseUrl ??= 'https://5d42a6e2bc64f90014a56ca0.mockapi.io/api/v1/';
   }
 
   final Dio _dio;
 
-  String baseUrl = 'https://5d42a6e2bc64f90014a56ca0.mockapi.io/api/v1/';
+  String baseUrl;
 
   @override
   getTasks() async {
