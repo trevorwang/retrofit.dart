7d0b737be22fbd7851f378fcdbf928cd5afca1b1
Trevor Wang <Trevor.Wang@qq.com>
2020-03-18 14:35:59 +0800

    fix: correct sample code error
    close #161

--- flutter_example/lib/example.dart
+++ flutter_example/lib/example.dart
@@ -1,9 +1,7 @@
-import 'dart:convert';
-
 import 'package:dio_http_cache/dio_http_cache.dart';
+import 'mock_adapter.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:dio/dio.dart' hide Headers;
-import 'package:dio/src/headers.dart' as gg;
 
 part 'example.g.dart';
 
@@ -16,21 +14,17 @@ abstract class RestClient {
 
 void test() {
   final dio = Dio();
+  dio.httpClientAdapter = MockAdapter();
+
   dio.interceptors.add(InterceptorsWrapper(onRequest: (options) {
     print(options.extra);
     print(options.baseUrl);
     print(options.method);
     print(options.headers);
-
-    return dio.resolve(Response(
-        data: jsonEncode([]),
-        headers: gg.Headers()
-          ..add("content-type", gg.Headers.jsonContentType)));
-  }, onResponse: (i) {
-    return jsonEncode(["hello", " world"]);
   }));
-  final api = RestClient(dio);
-  final options = buildCacheOptions(Duration(days: 7));
-  dio.options.extra.addAll(options.extra);
-  api.getTags().then(print).catchError(print);
+  final api = RestClient(dio, baseUrl: MockAdapter.mockBase);
+  final options = buildCacheOptions(Duration(days: 10));
+  api.getTags(options: options).then((it) {
+    print(it.length);
+  });
 }
