6c06ca433c11f1dff03ef592b6d9305d740b6e53
sooxt98 <sooxiaotong@gmail.com>
2021-03-22 13:56:44 +0800

    Added FormData.fromMap() for MultiPart support (#318)
    * :zap: Added FormData.fromMap() for MultiPart
    
    * Check type instead param name
    
    * Update generator_test_src.dart
    
    Co-authored-by: Trevor Wang <trevor.wang@qq.com>

--- generator/lib/src/generator.dart
+++ generator/lib/src/generator.dart
@@ -1016,6 +1016,14 @@ class RetrofitGenerator extends GeneratorForAnnotation<retrofit.RestApi> {
 
     final parts = _getAnnotations(m, retrofit.Part);
     if (parts.isNotEmpty) {
+      if (m.parameters.length == 1 && m.parameters.first.type.isDartCoreMap) {
+        blocks.add(refer('FormData')
+            .newInstanceNamed('fromMap',
+                [CodeExpression(Code(m.parameters.first.displayName))])
+            .assignFinal(_dataVar)
+            .statement);
+        return;
+      }
       blocks.add(
           refer('FormData').newInstance([]).assignFinal(_dataVar).statement);
 
