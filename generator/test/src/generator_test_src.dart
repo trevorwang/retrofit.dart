33315f008d664581fcd8c0e4d5851324a84c868c
yusuf <yusuf@setel.my>
2022-01-20 16:42:31 +0800

    feat: change `newRequestOptions` argument to Object. - `RequestOptions` does not extends or implements `Options`.
    - This allow to also pass `RequestOptions` which also take `CancelToken` as argument.

--- generator/test/src/generator_test_src.dart
+++ generator/test/src/generator_test_src.dart
@@ -806,29 +806,29 @@ abstract class TestModelList {
 }
 
 @ShouldGenerate(r'''
-  RequestOptions newRequestOptions(Options? options) {
+  RequestOptions newRequestOptions(Object? options) {
     if (options is RequestOptions) {
       return options as RequestOptions;
     }
-    if (options == null) {
-      return RequestOptions(path: '');
+    if (options is Options) {
+      return RequestOptions(
+        method: options.method,
+        sendTimeout: options.sendTimeout,
+        receiveTimeout: options.receiveTimeout,
+        extra: options.extra,
+        headers: options.headers,
+        responseType: options.responseType,
+        contentType: options.contentType.toString(),
+        validateStatus: options.validateStatus,
+        receiveDataWhenStatusError: options.receiveDataWhenStatusError,
+        followRedirects: options.followRedirects,
+        maxRedirects: options.maxRedirects,
+        requestEncoder: options.requestEncoder,
+        responseDecoder: options.responseDecoder,
+        path: '',
+      );
     }
-    return RequestOptions(
-      method: options.method,
-      sendTimeout: options.sendTimeout,
-      receiveTimeout: options.receiveTimeout,
-      extra: options.extra,
-      headers: options.headers,
-      responseType: options.responseType,
-      contentType: options.contentType.toString(),
-      validateStatus: options.validateStatus,
-      receiveDataWhenStatusError: options.receiveDataWhenStatusError,
-      followRedirects: options.followRedirects,
-      maxRedirects: options.maxRedirects,
-      requestEncoder: options.requestEncoder,
-      responseDecoder: options.responseDecoder,
-      path: '',
-    );
+    return RequestOptions(path: '');
   }
 ''', contains: true)
 @ShouldGenerate(r'''
