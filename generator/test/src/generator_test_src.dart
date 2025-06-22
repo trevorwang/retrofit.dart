442bb5f8240c3924e6736b566675649dc00cc005
Vasiliy Ditsyak <vasilich6107@users.noreply.github.com>
2025-06-22 16:40:29 +0200

    add unused_element_parameter to ignore section

--- generator/test/src/generator_test_src.dart
+++ generator/test/src/generator_test_src.dart
@@ -242,7 +242,7 @@ abstract class MultipleTypedExtrasTest {
 
 @ShouldGenerate(
   '''
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations,unused_element_parameter
 
 class _RestClient implements RestClient {
   _RestClient(this._dio, {this.baseUrl, this.errorLogger});
