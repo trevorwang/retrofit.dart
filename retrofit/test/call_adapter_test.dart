49cc00436f0719cff45ee60cf5155d485523589f
Trevor Wang <trevor.wang@qq.com>
2025-08-28 16:19:35 +0000

    test: increase coverage for dio.dart and call_adapter.dart

--- retrofit/test/call_adapter_test.dart
+++ retrofit/test/call_adapter_test.dart
@@ -9,12 +9,32 @@ class TestCallAdapter extends CallAdapter<String, int> {
   }
 }
 
+class DummyAdapter extends CallAdapter<int, String> {
+  @override
+  String adapt(int Function() call) {
+    return call().toString();
+  }
+}
+
 void main() {
   group('CallAdapter', () {
     test('adapts call correctly', () {
       final adapter = TestCallAdapter();
       final adapted = adapter.adapt(() => '42');
       expect(adapted, 42);
+    });
+
+    test('adapts int to string', () {
+      final adapter = DummyAdapter();
+      final adapted = adapter.adapt(() => 123);
+      expect(adapted, '123');
+    });
+  });
+
+  group('UseCallAdapter', () {
+    test('annotation stores type', () {
+      final use = UseCallAdapter(DummyAdapter);
+      expect(use.callAdapter, DummyAdapter);
     });
   });
 }
--- retrofit/test/dio_test.dart
+++ retrofit/test/dio_test.dart
@@ -3,9 +3,25 @@ import 'package:retrofit/dio.dart';
 
 void main() {
   group('Dio Annotations', () {
-    test('Extra stores data', () {
-      final extra = Extra({'foo': 'bar'});
-      expect(extra.data['foo'], 'bar');
+    test('Extra stores data and equality', () {
+      final extra1 = Extra({'foo': 'bar'});
+      final extra2 = Extra({'foo': 'bar'});
+      expect(extra1.data['foo'], 'bar');
+      expect(extra1, isA<Extra>());
+      expect(extra1.data, isA<Map<String, Object>>());
+      // Not equal because different instances
+      expect(extra1 == extra2, isFalse);
+    });
+    test('TypedExtras subclass', () {
+      class MyExtras extends TypedExtras {
+        const MyExtras();
+      }
+      const myExtras = MyExtras();
+      expect(myExtras, isA<TypedExtras>());
+    });
+    test('Extras is const and type', () {
+      const extras = Extras();
+      expect(extras, isA<Extras>());
     });
 
     test('TypedExtras and Extras are const', () {
