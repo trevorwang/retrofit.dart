00af192e1b01ecae12bad3e1366da4bb5427a791
Yusuf İhsan Görgel <developeryusuf@icloud.com>
2026-07-13 12:05:15 +0300

    fix(generator): null-safe contentType in newRequestOptions (Closes #917)

--- generator/test/src/generator_test_src.dart
+++ generator/test/src/generator_test_src.dart
@@ -1694,7 +1694,7 @@ abstract class TestModelList {
         extra: options.extra,
         headers: options.headers,
         responseType: options.responseType,
-        contentType: options.contentType.toString(),
+        contentType: options.contentType?.toString(),
         validateStatus: options.validateStatus,
         receiveDataWhenStatusError: options.receiveDataWhenStatusError,
         followRedirects: options.followRedirects,
